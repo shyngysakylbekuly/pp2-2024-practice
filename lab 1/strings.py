355d7601b33ec099f87363e665f7193caff0e219
Your name <your_email@whatever.com>
2024-01-20 18:12:03 +0600

    added strings

---
+++ lab 1/strings.py
@@ -0,0 +1,32 @@
+#ex 1
+x="Hello world"
+print(len(x))
+
+#ex 2
+txt="Hello World"
+x=txt[0]
+
+#ex 3
+txt="Hello World"
+x=txt[2:5]
+
+#ex 4
+txt=" Hello World "
+x=txt.strip()
+
+#ex 5
+txt="Hello World"
+txt=txt.upper()
+
+#ex 6
+txt="Hello World"
+txt=txt.lower()
+
+#ex 7
+txt="Hello World"
+txt=txt.replace("H","J")
+
+#ex 8
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
