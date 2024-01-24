7a61e73babbf4d15fcb8aeb7817a88fbd2a4e7fd
Your name <your_email@whatever.com>
2024-01-24 11:51:03 +0600

    added variablesexamples

---
+++ lab 1/variablesexamples.py
@@ -0,0 +1,29 @@
+#example 1
+x=6
+y="china"
+print(x)
+print(y)
+#6
+#china
+
+#example 2
+x=14
+x="marocco"
+#marocco
+
+#example 3
+x=str(7)
+y=int(7)
+z=float(7)
+#7
+#7
+#7.0
+
+#example 4
+x=13
+y="KBTU"
+print(type(x))
+print(type(y))
+#<class 'int'>
+#<class 'str'>
+
