fca497bc5add3286c0d0da4481f4895da6399c2d
Your name <your_email@whatever.com>
2024-01-24 11:55:44 +0600

    added datatypesexamples

---
+++ lab 1/datatypesexamples.py
@@ -0,0 +1,19 @@
+#example 1
+x=6
+print(type(x))
+#<class 'int'>
+
+#example 2
+x=float(29.5)
+print(type(x))
+#float
+
+#example 3
+x = list(("apple", "banana", "pineapple"))
+print(type(x))
+#list
+
+#example 4
+x=range(17)
+print(type(x))
+#range
