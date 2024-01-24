4ba050db10bf5472107749a9ad977025eb2c4d07
Your name <your_email@whatever.com>
2024-01-24 12:25:30 +0600

    added stringsexamples

---
+++ lab 1/stringsexamples.py
@@ -0,0 +1,33 @@
+#example 1
+print("Hello Kazakhstan")
+print('Hello Kazakhstan')
+#Hello Kazakhstan
+#Hello Kazakhstan
+
+#example 2
+x="China"
+print(x)
+#China
+
+#example 3
+x='''asasdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsds
+dsdsdsdsdsdsdsdsdsdsdsdsd '''
+print(x)
+'''asasdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsds
+dsdsdsdsdsdsdsdsdsdsdsdsd'''
+
+#example 4
+x="hello bro"
+print(x[3])
+#l
+
+#example 5
+for x in cherry:
+    print(x)
+#c
+#h
+#e
+#r
+#r
+#y
+                           
