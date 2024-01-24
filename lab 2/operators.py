fd51c84e1c30cd57df4f156afb9d3b43d591388d
Your name <your_email@whatever.com>
2024-01-24 12:41:50 +0600

    added operators

---
+++ lab 2/operators.py
@@ -0,0 +1,28 @@
+#ex 1
+print(10+5)
+15
+
+#ex 2
+print(10/2)
+5
+
+#ex 3
+fruits = ["apple", "banana"]
+if "apple" in fruits: 
+
+  print("Yes, apple is a fruit!")
+  #Yes,apple is a fruit
+
+  
+#ex 4
+if 5!=10:
+  print("5 and 10 are not equal")
+# 5 and 10 are not equal
+
+
+#ex 5
+if 5==10 or 4==4:
+  print("At least one of the statements is true")
+#At least one of the statements si true
+         
+      
---
+++ lab 2/operatorsexamples.py
@@ -0,0 +1,16 @@
+#examplee e1
+print(10+67)
+#77
+
+#example 2
+print(2+3+4+5+6-7-19)
+#-6
+
+#example 3
+print(2**3)
+#8
+
+#example 4
+print(10%2)
+#0
+
