437b96e700df25c06607b8428f9e6b6202630266
Your name <your_email@whatever.com>
2024-01-28 16:13:55 +0600

    added tuple

---
+++ lab 2/tupleexamples.py
@@ -0,0 +1,12 @@
+#example 1
+tttuple=("pp","stat","cherry")
+print(tttuple)
+
+#example 2
+ttuple=("pp","cc","hh","rr")
+print(len(ttuple))
+
+#example 3
+ttuple=("pp","cc","hh","rr","tt")
+print(type(ttuple))
+
