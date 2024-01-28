cc0c4b15ef3c0ea64f434c6fe3dc1824caf88e7c
Your name <your_email@whatever.com>
2024-01-28 15:58:18 +0600

    added list

---
+++ lab 2/listexamples.py
@@ -0,0 +1,27 @@
+#example 1
+thatlist=["pp","stat","math"]
+print(thatlist)
+#['pp','stat','math']
+
+#example 2
+thatlist=["pp","stat","math"]
+print(len(thatlist))
+#3
+
+#example 3
+list1=["pp","stat","math"]
+list2=[1,2,3,4,5,6,7,8]
+list3=[True,False]
+print(list1)
+print(list2)
+print(list3)
+#['pp','stat','math']
+#[1,2,3,4,5,6,7,8]
+#[True,False]
+
+#example 4
+mylist=["apple","pineapple","mango"]
+print(type(mylist))
+#<class 'list'>
+
+
